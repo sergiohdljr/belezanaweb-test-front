8e6c320ff3aff98afe71aa165ad771720c57c353
sergiohdljr <sergio.tjf1@gmail.com>
2023-07-29 15:40:48 -0300

    fix: header size

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,6 +1,6 @@
 export const Navigation = () => {
   return (
-    <header className="bg-white w-full h-10 flex justify-center shadow  ">
+    <header className="bg-white w-full h-16 flex justify-center shadow  ">
       <nav className="w-fit flex gap-3 items-center">
         <button className="text-gray-400 p-1">Sacola</button>
         <button className="text-gray-400 p-1">Pagamento</button>
