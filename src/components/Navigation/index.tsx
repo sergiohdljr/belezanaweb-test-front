0d455b78ec33957e259506720dd56fe4100fe758
sergiohdljr <sergio.tjf1@gmail.com>
2023-07-29 16:27:30 -0300

    fix: navigatin border-bottom

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,8 +1,10 @@
 export const Navigation = () => {
   return (
     <header className="bg-white w-full h-16 flex justify-center shadow  ">
-      <nav className="w-fit flex gap-3 items-center">
-        <button className="text-gray-400 p-1">Sacola</button>
+      <nav className="w-fit flex gap-3 items-stretch border-black">
+        <button className="text-gray-400 p-1 border-b-2 border-black">
+          Sacola
+        </button>
         <button className="text-gray-400 p-1">Pagamento</button>
         <button className="text-gray-400 p-1">Confirmação</button>
       </nav>
