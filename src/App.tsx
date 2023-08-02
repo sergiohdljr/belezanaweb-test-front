7cfffeac538a490831538fa57c5b72352d36dacd
sergiohdljr <sergio.tjf1@gmail.com>
2023-08-02 11:27:39 -0300

    feat: set active children to template page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import { Bag, PaymentDetails } from "./components";
+import { Bag, PaymentDetails, Product } from "./components";
 import { Form } from "./components/Form/Form";
 import { LayoutApp } from "./Layouts";
+import { NavStore } from "./store";
 
 export const App = () => {
+  const { active } = NavStore();
+
+  const arrTemp = [1, 2, 3];
+
+  const Produtos = arrTemp.map((produto) => <Product />);
+
+  const ActiveNav =
+    active === "Sacola" ? Produtos : active === "Pagamento" ? <Form /> : null;
+
   return (
     <LayoutApp>
-      <Bag>
-        <Form />
-      </Bag>
+      <Bag>{ActiveNav}</Bag>
       <PaymentDetails />
     </LayoutApp>
   );
--- src/components/Bag/index.tsx
+++ src/components/Bag/index.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from "react";
 
 interface ContainerProps {
-  children: ReactNode[];
+  children: ReactNode[] | ReactNode;
 }
 
 export const Bag = ({ children }: ContainerProps) => {
